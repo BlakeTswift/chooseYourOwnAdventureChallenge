d73fdb90954092ce1c0486349a273d3ecac839fb
BlakeT <BlakeTswift@gmail.com>
2025-10-03 14:09:10 -0600

    Initial Commit

---
+++ chooseYourOwnAdventureChallenge/chooseYourOwnAdventureChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  chooseYourOwnAdventureChallengeApp.swift
+//  chooseYourOwnAdventureChallenge
+//
+//  Created by Trytten, Blake - Student on 10/3/25.
+//
+
+import SwiftUI
+
+@main
+struct chooseYourOwnAdventureChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ chooseYourOwnAdventureChallengeTests/chooseYourOwnAdventureChallengeTests.swift
@@ -0,0 +1,17 @@
+//
+//  chooseYourOwnAdventureChallengeTests.swift
+//  chooseYourOwnAdventureChallengeTests
+//
+//  Created by Trytten, Blake - Student on 10/3/25.
+//
+
+import Testing
+@testable import chooseYourOwnAdventureChallenge
+
+struct chooseYourOwnAdventureChallengeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
